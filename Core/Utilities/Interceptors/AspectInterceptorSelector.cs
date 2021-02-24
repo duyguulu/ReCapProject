553f678d86b7f5c0d3e18333bb929c6d3356330c
duyguulu <d.ulu.duygu@gmail.com>
2021-02-24 08:49:14 +0300

    Autofac, FluentValidation, AOP' nın eklenmesi.

---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,25 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+	public class AspectInterceptorSelector : IInterceptorSelector
+	{
+		public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+		{
+			var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+				(true).ToList();
+			var methodAttributes = type.GetMethod(method.Name)
+				.GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+			classAttributes.AddRange(methodAttributes);
+			//burası tüm methodlara loglama yapılmasını tetikler, su an loglama hazır değil yorum satırına aldım
+			//classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));
+
+			return classAttributes.OrderBy(x => x.Priority).ToArray();
+		}
+	}
+}
